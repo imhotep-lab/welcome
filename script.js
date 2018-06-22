35568d40a9a3dcc089010510b026a8c121c6521c
krishna <krishna@tttm>
2018-06-22 18:45:40 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//aumosiris@gmail.com
+
+function sendMail() {
+    var link = "mailto:aumosiris@gmail.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + escape("This is my subject")
+             + "&body=" + escape(document.getElementById('myText').value)
+    ;
+
+    window.location.href = link;
+}
+
+
